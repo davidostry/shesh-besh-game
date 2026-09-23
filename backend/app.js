56009141b12f403925d56966639d14c094414eab
david <sarigoo421@gmail.com.il>
2026-09-23 12:41:46 +0300

    written app file

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+import { log } from 'console';
+import express from 'express';
+import { createServer } from 'http';
+import {Server} from 'socket.io'
+
+
+const app = express()
+const server = createServer(app)
+const io = new Server({cors:{origin:"http://localhost:5173"}});
+
+io.on("connect", (user)=>{
+    console.log("player connected", user.id);
+    
+})
+
+io.on("disconnect", (user)=>{
+    console.log("player disconnected", user.id);
+})
+
+server.listen(8080, ()=>{
+    console.log("server is runing on http:localhost:8080");
+    
+})
